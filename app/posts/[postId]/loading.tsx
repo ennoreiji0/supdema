8487430169038fc7b81434d9df332d37fba1844a
ennoreiji0 <ennoreiji0@gmail.com>
2026-04-20 16:19:21 +0900

    error3

--- app/posts/[postId]/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
-      <p className="ml-4">読み込み中...</p>
-    </div>
-  )
-}
